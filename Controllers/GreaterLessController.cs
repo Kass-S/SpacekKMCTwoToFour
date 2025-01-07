33d284f9a0948fb5bc746311e253fa021c26a802
Kass-S <kassiespacek101@gmail.com>
2025-01-07 14:16:23 -0800

    updated logic on GreaterLess endpoint

--- Controllers/GreaterLessController.cs
+++ Controllers/GreaterLessController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SpacekKMCTwoToFour.Services;
 
 namespace SpacekKMCTwoToFour.Controllers
 {
@@ -10,6 +11,17 @@ namespace SpacekKMCTwoToFour.Controllers
     [Route("[controller]")]
     public class GreaterLessController : ControllerBase
     {
-        
+        private readonly GreaterLessServices _greaterLessServices;
+        public GreaterLessController(GreaterLessServices greaterLessServices){
+            _greaterLessServices = greaterLessServices;
+        }
+
+        [HttpGet]
+        [Route("Compare/{num1}/{num2}")]
+
+        public string GreaterLess(int num1, int num2)
+        {
+            return _greaterLessServices.GreaterLess(num1, num2);
+        }
     }
 }
--- Services/GreaterLessServices.cs
+++ Services/GreaterLessServices.cs
@@ -7,6 +7,21 @@ namespace SpacekKMCTwoToFour.Services
 {
     public class GreaterLessServices
     {
-        
+        public string GreaterLess(int num1, int num2)
+        {
+            string greaterLessStr = "";
+            if(num1 > num2)
+            {
+                greaterLessStr = $"{num1} is greater than {num2}. {num2} is less than {num1}.";
+            }else if(num1 < num2)
+            {
+                greaterLessStr = $"{num2} is greater than {num1}. {num1} is less than {num2}.";
+            }else if(num1 == num2)
+            {
+                greaterLessStr = $"{num1} and {num2} are equal.";
+            }
+
+            return greaterLessStr;
+        }
     }
 }
